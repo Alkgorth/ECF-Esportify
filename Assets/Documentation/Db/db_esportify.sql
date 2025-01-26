8d01a9fba21f5a6a0a65312360723a24a3c353f0
Alkgorth <ap.percheron@gmail.com>
2025-01-26 20:41:27 +0100

    Modifications de la base données

--- Assets/Documentation/Db/db_esportify.sql
+++ Assets/Documentation/Db/db_esportify.sql
@@ -1,77 +1,86 @@
-CREATE TABLE USERS(
-   id_user INT,
-   last_name VARCHAR(255) NOT NULL,
-   first_name VARCHAR(255) NOT NULL,
-   mail VARCHAR(255) NOT NULL,
-   pseudo VARCHAR(124) NOT NULL,
-   password VARCHAR(255) NOT NULL,
-   role VARCHAR(14) NOT NULL,
-   PRIMARY KEY(id_user),
-   UNIQUE(mail),
-   UNIQUE(pseudo)
-);
+USE esportify_bdd;
+CREATE TABLE `user`(
+   `id_user` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   `last_name` varchar(255) NOT NULL,
+   `first_name` varchar(255) NOT NULL,
+   `mail` varchar(255) NOT NULL UNIQUE,
+   `pseudo` varchar(124) NOT NULL UNIQUE,
+   `password` varchar(255) NOT NULL,
+   `role` ENUM('joueur','organisateur', 'administrateur') NOT NULL DEFAULT 'joueur'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE TOKENS(
-   id_token INT,
-   creation_date DATETIME NOT NULL,
-   expiration_date DATETIME NOT NULL,
-   token VARCHAR(255) NOT NULL,
-   fk_id_user INT NOT NULL,
-   PRIMARY KEY(id_token),
-   UNIQUE(token)
-);
+CREATE TABLE `tokens`(
+   `id_token` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   `creation_date` datetime DEFAULT CURRENT_TIMESTAMP,
+   `expiration_date` datetime NOT NULL,
+   `token` VARCHAR(255) NOT NULL UNIQUE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE FAVORITES(
-   id_favorite INT,
-   name VARCHAR(255) NOT NULL,
-   fk_id_user INT NOT NULL,
-   id_user INT,
-   PRIMARY KEY(id_favorite),
-   FOREIGN KEY(id_user) REFERENCES USERS(id_user)
-);
+CREATE TABLE `favorite`(
+   `id_favorite` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   `name` varchar(255) NOT NULL,
+   `status` ENUM ('en attente', 'validé', 'refusé') NOT NULL,
+   `fk_id_user` int(11) UNSIGNED NOT NULL,
+   FOREIGN KEY(fk_id_user) REFERENCES user(id_user) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE PLATEFORME(
-   id_plateforme INT,
-   name VARCHAR(255) NOT NULL,
-   fk_id_event INT NOT NULL,
-   PRIMARY KEY(id_plateforme)
-);
+sql CREATE TABLE `favorite` (
+   `id_favorite` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   `fk_id_user` INT(11) UNSIGNED NOT NULL,
+   `fk_id_event` INT(11) UNSIGNED NOT NULL,
+   FOREIGN KEY (fk_id_user) REFERENCES user(id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+   FOREIGN KEY (fk_id_event) REFERENCES event(id_event) ON DELETE CASCADE ON UPDATE CASCADE,
+   UNIQUE KEY (fk_id_user, fk_id_event) 
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE VISIBILITY(
-   id_visibility INT,
-   name VARCHAR(50) NOT NULL,
-   fk_id_event INT NOT NULL,
-   PRIMARY KEY(id_visibility)
-);
+CREATE TABLE `plateforme`(
+   `id_plateforme` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   `name` varchar(255) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE EVENTS(
-   id_event INT,
-   name_event VARCHAR(255) NOT NULL,
-   name_game VARCHAR(255) NOT NULL,
-   date_hour_start DATETIME NOT NULL,
-   date_hour_end DATETIME NOT NULL,
-   description TEXT NOT NULL,
-   fk_id_user INT NOT NULL,
-   id_visibility INT NOT NULL,
-   id_user INT NOT NULL,
-   PRIMARY KEY(id_event),
-   UNIQUE(name_event),
-   FOREIGN KEY(id_visibility) REFERENCES VISIBILITY(id_visibility),
-   FOREIGN KEY(id_user) REFERENCES USERS(id_user)
-);
+CREATE TABLE `event`(
+   `id_event` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   `name_event` varchar(255) NOT NULL,
+   `name_game` varchar(255) NOT NULL,
+   `date_hour_start` datetime NOT NULL,
+   `date_hour_end` datetime NOT NULL,
+   `nombre_de_joueurs` int(11) UNSIGNED NOT NULL,
+   `description` text NOT NULL,
+   `visibility` ENUM('public', 'privé') NOT NULL DEFAULT 'public',
+   `fk_id_favorite` int(11) UNSIGNED NOT NULL,
+   `fk_id_user` int(11) UNSIGNED NOT NULL,
+   FOREIGN KEY(fk_id_favorite) REFERENCES favorite(id_favorite) ON DELETE CASCADE ON UPDATE CASCADE,
+   FOREIGN KEY(fk_id_user) REFERENCES user(id_user) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE PASSWORD(
-   id_user INT,
-   id_token INT,
-   PRIMARY KEY(id_user, id_token),
-   FOREIGN KEY(id_user) REFERENCES USERS(id_user),
-   FOREIGN KEY(id_token) REFERENCES TOKENS(id_token)
-);
+CREATE TABLE `password`(
+   `fk_id_user` int(11) UNSIGNED NOT NULL,
+   `fk_id_token` int(11) UNSIGNED NOT NULL,
+   PRIMARY KEY (fk_id_user, fk_id_token),
+   FOREIGN KEY(fk_id_user) REFERENCES user(id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+   FOREIGN KEY(fk_id_token) REFERENCES tokens(id_token) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE SPECIFICATION(
-   id_event INT,
-   id_plateforme INT,
-   PRIMARY KEY(id_event, id_plateforme),
-   FOREIGN KEY(id_event) REFERENCES EVENTS(id_event),
-   FOREIGN KEY(id_plateforme) REFERENCES PLATEFORME(id_plateforme)
-);
+CREATE TABLE `scores`(
+   `fk_id_user` int(11) UNSIGNED NOT NULL,
+   `fk_id_event` int(11) UNSIGNED NOT NULL,
+   PRIMARY KEY(id_user, id_event),
+   `score` int(11) UNSIGNED NOT NULL CHECK (score >=0),
+   `score_total` decimal(10,2) UNSIGNED NOT NULL CHECK (score_total >= score),
+   FOREIGN KEY(fk_id_user) REFERENCES user(id_user),
+   FOREIGN KEY(fk_id_event) REFERENCES event(id_event)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `specification`(
+   `fk_id_event` int(11) UNSIGNED NOT NULL,
+   `fk_id_plateforme` int(11) UNSIGNED NOT NULL,
+   PRIMARY KEY (fk_id_event, fk_id_plateforme),
+   FOREIGN KEY(fk_id_event) REFERENCES event(id_event) ON DELETE CASCADE ON UPDATE CASCADE,
+   FOREIGN KEY(fk_id_plateforme) REFERENCES plateforme(id_plateforme) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE INDEX idx_user ON user (mail, pseudo);
+CREATE INDEX idx_event_user ON event (fk_id_user);
+CREATE INDEX idx_event_visibility ON event (visibility);
+CREATE INDEX idx_score_event ON event (fk_id_event);
+CREATE UNIQUE INDEX idx_token_token ON tokens (token);
