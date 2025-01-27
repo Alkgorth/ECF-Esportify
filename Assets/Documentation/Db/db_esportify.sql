dc647e39e764b39dbf2cebe1a69deabed02e9dff
Alkgorth <ap.percheron@gmail.com>
2025-01-27 13:32:52 +0100

    modification sur la table event, et suppression de la table spécification

--- Assets/Documentation/Db/db_esportify.sql
+++ Assets/Documentation/Db/db_esportify.sql
@@ -1,4 +1,5 @@
 USE esportify_bdd;
+
 CREATE TABLE `user`(
    `id_user` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
    `last_name` varchar(255) NOT NULL,
@@ -39,9 +40,11 @@ CREATE TABLE `event`(
    `nombre_de_joueurs` int(11) UNSIGNED NOT NULL,
    `description` text NOT NULL,
    `visibility` ENUM('public', 'privé') NOT NULL DEFAULT 'public',
-   `status` ENUM ('en attente', 'validé', 'refusé') NOT NULL,
+   `status` ENUM ('en attente', 'validé', 'refusé', 'annulé') NOT NULL DEFAULT 'en attente',
    `fk_id_user` int(11) UNSIGNED NOT NULL,
-   FOREIGN KEY(fk_id_user) REFERENCES user(id_user) ON DELETE CASCADE ON UPDATE CASCADE
+   `fk_id_plateforme` int(11) UNSIGNED NOT NULL,
+   FOREIGN KEY(fk_id_user) REFERENCES user(id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+   FOREIGN KEY(fk_id_plateforme) REFERENCES plateforme(id_plateforme) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `password`(
@@ -62,16 +65,9 @@ CREATE TABLE `scores`(
    FOREIGN KEY(fk_id_event) REFERENCES event(id_event)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `specification`(
-   `fk_id_event` int(11) UNSIGNED NOT NULL,
-   `fk_id_plateforme` int(11) UNSIGNED NOT NULL,
-   PRIMARY KEY (fk_id_event, fk_id_plateforme),
-   FOREIGN KEY(fk_id_event) REFERENCES event(id_event) ON DELETE CASCADE ON UPDATE CASCADE,
-   FOREIGN KEY(fk_id_plateforme) REFERENCES plateforme(id_plateforme) ON DELETE CASCADE ON UPDATE CASCADE
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 CREATE INDEX idx_user ON user (mail, pseudo);
 CREATE INDEX idx_event_user ON event (fk_id_user);
 CREATE INDEX idx_event_visibility ON event (visibility);
+CREATE INDEX idx_event_date_start ON event (date_hour_start);
 CREATE INDEX idx_score_event ON scores (fk_id_event);
 CREATE UNIQUE INDEX idx_token_token ON tokens (token);
