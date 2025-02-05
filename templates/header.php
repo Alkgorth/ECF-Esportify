e23d2971da5a7e240a36c7d0f7033d95aab43740
Alkgorth <ap.percheron@gmail.com>
2025-02-05 17:34:45 +0100

    maj des settings sass et css

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,10 @@ use App\Tools\UserValidator;
 ?>
 <header>
     <nav>
-        <section>
+
+        <!-- BARRE DE NAVIGATION GAMESTORE -->
+
+        <!-- <section>
             <aside>
                 <div class="d-flex flex-column flex-shrink-0 bg-light" style="width: 4.5rem">
                     <a href="/" class="d-block p-3 link-dark text-decoration-none" title="Icon-only" data-bs-toggle="tooltip" data-bs-placement="right">
@@ -70,44 +73,112 @@ use App\Tools\UserValidator;
                 </div>
             </aside>
         </section>
-    </nav>
+    </nav> -->
 
-</header>
 
-<div id="nav-latteral" class="nav-latteral">
-    <div id="nav-close" class="nav-button">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+        <!-- BARRE DE NAVIGATION TUTO YOUTUBE -->
+
+
+        <!-- <div id="nav nav-latteral" class="nav-latteral position-absolute top-0 bg-white z-3">
+    <div id="nav-close" class="nav-button mb-4">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
         </svg>
     </div>
-    <div class="nav-links">
-        <div class="nav-link">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+    <div class="nav-links pt-1 pe-6 ps-1 pb-3">
+        <div class="nav-link d-flex fs-3 align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
                 <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
             </svg>
             Accueil
         </div>
-        <div class="nav-link">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+        <div class="nav-link d-flex fs-3 align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
             </svg>
             Compte
         </div>
-        <div class="nav-link">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+        <div class="nav-link d-flex fs-3 align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
                 <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
             </svg>
             Paramètres
         </div>
     </div>
 </div>
-<div id="nav-wrapper"></div>
-<nav>
+<div id="nav-wrapper" class="position-absolute top-0 start-0 z-2 d-none"></div>
+<nav class="position-absolute top-0 start-0">
     <div id="nav-open" class="nav-button">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
         </svg>
-    </div>
+    </div> -->
 
-</nav>
+
+
+        <!-- BARRE DE NAVIGATION BOOTSTRAP -->
+
+        <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+      <svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      <span class="fs-5 fw-semibold">Collapsible</span>
+    </a>
+    <ul class="list-unstyled ps-0">
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+          Home
+        </button>
+        <div class="collapse show" id="home-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark rounded">Overview</a></li>
+            <li><a href="#" class="link-dark rounded">Updates</a></li>
+            <li><a href="#" class="link-dark rounded">Reports</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+          Dashboard
+        </button>
+        <div class="collapse" id="dashboard-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark rounded">Overview</a></li>
+            <li><a href="#" class="link-dark rounded">Weekly</a></li>
+            <li><a href="#" class="link-dark rounded">Monthly</a></li>
+            <li><a href="#" class="link-dark rounded">Annually</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+          Orders
+        </button>
+        <div class="collapse" id="orders-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark rounded">New</a></li>
+            <li><a href="#" class="link-dark rounded">Processed</a></li>
+            <li><a href="#" class="link-dark rounded">Shipped</a></li>
+            <li><a href="#" class="link-dark rounded">Returned</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="border-top my-3"></li>
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+          Account
+        </button>
+        <div class="collapse" id="account-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark rounded">New...</a></li>
+            <li><a href="#" class="link-dark rounded">Profile</a></li>
+            <li><a href="#" class="link-dark rounded">Settings</a></li>
+            <li><a href="#" class="link-dark rounded">Sign out</a></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+    </nav>
+</header>
--- templates/pages/home.php
+++ templates/pages/home.php
@@ -5,9 +5,71 @@ require_once _ROOTPATH_ . '\templates\header.php';
 
 ?>
 
-<main>
+<main class="w-100">
     <section>
-        <h1 class="text-align-center fs-4">Page d'Accueil</h1>
+        <h1 class="fs-4 text-center mt-2 mb-4">Page d'Accueil</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci culpa facere velit omnis officiis esse.
+            Quasi, odio animi magni deserunt, sint ipsam blanditiis placeat voluptates debitis officia quisquam nulla
+            aliquid adipisci nisi quis. Voluptatem quaerat facere rerum soluta ipsam nam dolorum, pariatur vel rem in
+            nesciunt ducimus, ipsum et? Sint, laboriosam suscipit nemo eius repellat nam impedit perferendis nostrum
+            accusamus, ducimus exercitationem aspernatur voluptatum veniam quidem! Delectus cum tempora temporibus.
+            Aliquid soluta assumenda nihil eligendi aliquam necessitatibus! Doloribus esse sapiente accusantium vitae
+            porro excepturi harum, provident facere. Quasi hic cumque vero odio minima, iusto enim ullam, sint vitae
+            accusantium, sed explicabo. Consequatur debitis deleniti, odit cupiditate, dignissimos maxime magni nesciunt
+            fugit natus quibusdam distinctio quod molestias modi veritatis inventore sint tempore aspernatur mollitia?
+            Ea eos quasi nulla beatae eligendi blanditiis repudiandae enim dolorum, ipsa molestiae amet, veniam numquam
+            quas voluptatem aliquam sed vitae dignissimos harum labore! Temporibus molestias nisi possimus ducimus libero,
+            reprehenderit odio nam illum corporis esse quasi deleniti eum repellat quos repellendus! Corrupti officia qui
+            voluptatibus consequatur maiores cupiditate odio? Ad maiores et aspernatur, aut doloremque fugiat quidem, eaque
+            impedit accusantium distinctio delectus sit nobis porro aliquid autem facere illo tenetur voluptatibus commodi
+            nemo. Totam rem magni reiciendis, id, quod perspiciatis ad cupiditate quae debitis, pariatur harum facere nobis.
+            Tempora natus necessitatibus atque, magni incidunt modi eum quis aperiam, nulla illum esse aspernatur provident
+            tempore ducimus sit maiores qui labore ea aut at porro sequi eaque. Excepturi aliquam non temporibus numquam eos
+            laboriosam ad neque labore voluptas dolores amet perspiciatis accusantium deleniti delectus cum, incidunt hic maxime
+            est nesciunt corporis ipsa, corrupti minima enim? Quas, corporis eum quisquam tenetur atque, consectetur ut aspernatur
+            est, reprehenderit similique beatae debitis magnam explicabo doloribus nihil consequatur asperiores nam aperiam.
+            Repellendus nostrum vero error aspernatur provident obcaecati blanditiis! Illum accusantium a debitis, aperiam veritatis
+            cupiditate molestiae at aspernatur tempora error adipisci, facere quae provident voluptatem. Excepturi, ipsa dolorum.
+            Velit, dolores exercitationem! Nihil fugit ullam sunt aspernatur numquam suscipit, harum quo, similique consequuntur odit
+            voluptatem enim temporibus laboriosam quibusdam commodi id alias unde delectus deleniti veritatis! Quibusdam, consequatur
+            inventore repellat autem fugiat maiores deleniti esse blanditiis voluptatem illum voluptate, reiciendis, ut laudantium veritatis
+            fuga. Nesciunt fugit libero iste qui! Reiciendis ad quo tempora saepe magni provident et quibusdam? Hic, error eveniet.
+            Sed, ullam nisi architecto quos fugiat quod quis labore iusto ut neque possimus unde est mollitia delectus praesentium.
+            Iure laboriosam unde dignissimos, cum eum, error repudiandae quasi ex iusto odit asperiores ullam atque explicabo quod
+            tempora odio! Accusantium consequatur distinctio, odit pariatur eveniet fugiat dignissimos, sed in impedit minima soluta
+            laboriosam iure rem vero velit laborum perferendis voluptatem maiores nisi aliquid. Debitis, itaque. Cupiditate odio
+            laborum nesciunt eius! Laboriosam consequatur quos, quae iste minus quidem ut reiciendis accusantium laudantium odio
+            blanditiis maxime in nisi quisquam dicta omnis voluptate saepe earum aliquid ipsa cumque neque odit qui recusandae.
+            Quis tempora quibusdam enim quia. Officia possimus omnis id tenetur, quaerat modi earum? Aut fugiat ipsa libero
+            neque vero quis quia quo aliquid reiciendis, sequi temporibus corrupti consectetur eligendi omnis ipsum, vel
+            facere fugit saepe eaque expedita repellendus similique nemo optio. Sunt enim quidem veniam deserunt aperiam
+            repudiandae minus eaque similique. Culpa ipsum rem eaque quis iste aut non at tempore ullam, recusandae natus dolore
+            sapiente id cum quisquam quas est autem minus amet exercitationem sint iusto accusamus sequi! Facilis consectetur consequuntur
+            ducimus, id eius blanditiis quibusdam pariatur adipisci asperiores dolore? Repellendus sint eligendi, earum in rerum dolor
+            laudantium voluptas, consectetur beatae deleniti saepe quia quisquam natus autem, praesentium at esse repudiandae neque laboriosam
+            minus ullam error quis quidem cumque? Accusantium, quaerat numquam alias autem tempora aperiam iste est mollitia ratione magni id,
+            at libero et ad! Animi repudiandae praesentium placeat earum necessitatibus excepturi optio possimus perferendis exercitationem
+            cumque nihil odio, aspernatur ullam fuga quidem accusantium aperiam totam assumenda voluptate? Eaque, molestiae aliquam ut iusto
+            sed aliquid? Nulla quasi, officia, in incidunt necessitatibus eaque enim cum repudiandae minima quibusdam dolorum esse! Aspernatur
+            dolor placeat pariatur sapiente eos eveniet laudantium explicabo tenetur consectetur reprehenderit ut vel unde asperiores delectus,
+            nulla facilis, tempora totam quis voluptate a ullam! Ad at ducimus aliquam ex in error quod harum labore! Quas inventore in officiis
+            ut saepe assumenda qui excepturi labore eius. Illum beatae, excepturi odit commodi cupiditate saepe eaque aut hic, deserunt
+            consequuntur, recusandae eos magni soluta mollitia ex fugiat? Voluptatum consequuntur porro autem velit, molestiae totam odit
+            optio mollitia iusto fuga corrupti. Facilis, dolore repellat enim, excepturi et impedit ratione ipsam cumque rem illum iste.
+            Deleniti sint ducimus placeat? Hic error quod aperiam repudiandae amet. Praesentium inventore voluptates hic cupiditate
+            architecto odit officia doloribus nulla nemo debitis! Nam, tenetur distinctio ipsa quis nihil saepe ab obcaecati fugit
+            sed similique voluptatum quae iure incidunt aperiam temporibus vitae, aut ratione! Iste tempore nobis unde, eligendi
+            aliquam laborum perferendis, vel vero quia tenetur quas in obcaecati iure nihil, amet placeat dolorum dolorem libero
+            maiores harum eaque omnis. Vitae beatae, cumque veritatis autem iusto nesciunt ducimus, ut officia repudiandae doloremque
+            unde error quis obcaecati fuga doloribus laudantium molestiae at sint magni voluptate molestias, dolore dolorum! Ratione
+            autem explicabo vel dignissimos est veritatis distinctio facilis adipisci, minima assumenda suscipit minus itaque! Autem nihil
+            quaerat, quod architecto distinctio blanditiis nemo dolorum eum nostrum, qui natus repudiandae officiis in iusto, molestiae impedit ratione v
+            oluptatem reiciendis illum ullam esse. Suscipit, voluptatem culpa aperiam obcaecati temporibus quasi iure fugit corrupti laboriosam molestiae iusto optio,
+            quisquam facere nobis nostrum ipsam voluptatibus, accusantium veniam! Iure voluptate fuga asperiores facere. Perspiciatis iusto sit itaque mollitia! Eos ex
+            eveniet quis quo enim animi, officia ullam modi nam provident molestiae reiciendis, exercitationem tenetur obcaecati doloremque ut neque distinctio ea ducimus
+            vitae cum? Dolore impedit cupiditate exercitationem dicta magnam molestias eius quaerat rem aliquam, quam sapiente incidunt saepe, ipsum recusandae perferendis
+            obcaecati tempore pariatur laborum eveniet hic! Et doloribus eum placeat vitae magni nesciunt? Minima quis debitis commodi voluptatibus nulla, non labore vero
+            voluptas magni vel doloribus id ratione minus aspernatur fugiat! Ab iste aut, suscipit rem hic nisi inventore veritatis debitis reiciendis.</p>
     </section>
 </main>
 
