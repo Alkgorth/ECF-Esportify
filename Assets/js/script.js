0e92250caeffcc4ccda0ee96078396bfd1703913
Alkgorth <ap.percheron@gmail.com>
2025-02-14 12:09:50 +0100

    Test js sur la barre de navigation

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -15,3 +15,20 @@ const closeNav = () => {
 openBtn.addEventListener("click", openNav);
 closeBtn.addEventListener("click", closeNav);
 navWrapper.addEventListener("click", closeNav);
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const navbar = document.querySelector(".navbar");
+//     const navBurger = document.getElementById("nav-burger");
+
+//     navBurger.addEventListener("click", function (event) {
+//         event.preventDefault();
+//         navbar.classList.toggle("active");
+//     });
+
+//     // Fermer le menu si on clique en dehors
+//     document.addEventListener("click", function (event) {
+//         if (!navbar.contains(event.target) && !navBurger.contains(event.target)) {
+//             navbar.classList.remove("active");
+//         }
+//     });
+// });
--- templates/header.php
+++ templates/header.php
@@ -54,7 +54,7 @@ use App\Tools\UserValidator;
                         <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="2.5em" height="2.5em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-search">
                             <path class="color-first color-second" d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                         </svg>
-                        <input class="form-control nav-text" type="search" placeholder="Search" aria-label="Search">
+                        <input class="form-control nav-text me-2" type="search" placeholder="Search" aria-label="Search">
                     </form>
                 </a>
             </li>
