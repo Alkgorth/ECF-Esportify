db161836dcbddd5fe6fadd3b4b3dbac801e21e47
Alkgorth <ap.percheron@gmail.com>
2025-02-27 22:40:31 +0100

    Maj responsive barre nav =<600px

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,23 +3,6 @@ console.log("Je suis chargé");
 const openBtn = document.getElementById("nav-burger");
 const navBar = document.getElementById("navbar");
 
-
-/*if (openBtn && navBar) {
-    openBtn.addEventListener('click', () => {
-        console.log("Click détecté !");
-        console.log("navBar:", navBar);
-        
-        if (navBar.classList) {  // Vérifie que classList existe
-            navBar.classList.toggle('visible');
-        } else {
-            console.error("L'élément navBar n'a pas classList !");
-        }
-    });
-} else {
-    console.error("L'élément navBar ou openBtn n'existe pas !");
-}*/
-
-
 openBtn.addEventListener('click', () => {
     const isNavVisible = navBar.classList.contains('visible');
 
@@ -27,6 +10,5 @@ openBtn.addEventListener('click', () => {
         navBar.classList.remove('visible');
     } else {
         navBar.classList.add('visible');
-
     }
 });
