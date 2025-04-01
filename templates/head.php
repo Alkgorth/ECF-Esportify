5a7b786a2e13832eddad2071f626eacdc5ae11a0
Alkgorth <ap.percheron@gmail.com>
2025-04-01 18:36:46 +0200

    Correcttion affichage des erreurs pour le formulaire event, désinstall de chart.js en local

--- templates/head.php
+++ templates/head.php
@@ -8,10 +8,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&family=Rajdhani&family=Roboto&display=swap" rel="stylesheet">
-
+    <!-- Links CSS -->
     <link rel="stylesheet" href="..\node_modules\bootstrap-icons\font\bootstrap-icons.min.css">
     <link rel="stylesheet" href="..\Assets\css\main.css">
-    
+    <!-- Links Chart -->
+     
     <title>Esportify</title>
 
 
