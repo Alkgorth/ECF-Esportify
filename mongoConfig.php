5a58c70c846a4293db7fcded761c2b4160aeb710
Alkgorth <ap.percheron@gmail.com>
2025-06-05 11:23:15 +0200

    Ajout connexion PHP -> MongoDB

---
+++ mongoConfig.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'mongo_host' => 'mongodb',
+    'mongo_port' => '27017',
+    'mongo_name' => 'esportify_mongo',
+    'mongo_user' => 'MonUtilisateur',
+    'mongo_password' => 'MonMotDePasse'
+];
